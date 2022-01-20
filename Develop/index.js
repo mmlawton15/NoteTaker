8ede15807d019e381a2f2d91cfaef9c9524c061f
Mary Margaret <mmlawton15@gmail.com>
2022-01-20 18:55:01 -0500

    setting up files before I revisit lesson content

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,12 @@
 //when this is deployed all of this will go into the root
-
-console.log("hello world");
 //all server code goes in here for starting the project and starting the public folder
-//create app const express = require etc app.listen etc, middleware etc, server knows to get json
+
    
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes'); //these will be different but similar. routes for views and database
+const htmlRoutes = require('./routes/htmlRoutes'); //these will be different but similar. database and views routes
 app.use(express.urlencoded({ extended: true })); //server can be a server
 app.use(express.json());//consume json
 app.use(express.static('public'));//serve files to the browser

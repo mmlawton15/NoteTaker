0bd5ea0b55f5b2d74fac7483de9c529807d377a6
Mary Margaret <mmlawton15@gmail.com>
2022-01-22 10:08:33 -0500

    i was so so close, got some help and now the old existing notes load on my pagegit add -A! YEA

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,24 @@
 const router = require('express').Router()
 const fs = require("fs");
 
-let preloadedNotes = fs.readFileSync('./db/db.json');
-let parsedNotes = JSON.parse(preloadedNotes);
+
 // someone is going to be calling for data at this endpoint
 // give them the data
 // how
 // where are the notes - db.json
 // reac into the db.json file
 // grabe the notes and return the json
+
+
 router.get('/notes', (req, res) => {
+    //read the db every time you hit a route, otherwise i will only be dealing with old notes and reading it when the server turns on
+    let preloadedNotes = fs.readFileSync('./db/db.json');
+    //parse the notes to be json
+    let parsedNotes = JSON.parse(preloadedNotes);
+    //return the item
+    
     console.log(parsedNotes); //returning db.json notes
-    res.json([{title: 'Test Title', text: 'Test Text'}])// placeholder
-    res.json([{parsedNotes(title)}]) //what i am working with
+    res.json(parsedNotes) //what i am working with
 });
 
 router.post('/notes', (req, res) => { 

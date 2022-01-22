b18c1827881c21f896044f991014dfc484e4d04d
Mary Margaret <mmlawton15@gmail.com>
2022-01-22 09:43:58 -0500

    got the code to show the parsed notes from db.json, just need to get them to load in the notes section of the page

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
+const fs = require("fs");
 
+let preloadedNotes = fs.readFileSync('./db/db.json');
+let parsedNotes = JSON.parse(preloadedNotes);
 // someone is going to be calling for data at this endpoint
 // give them the data
 // how
@@ -7,8 +10,8 @@ const router = require('express').Router()
 // reac into the db.json file
 // grabe the notes and return the json
 router.get('/notes', (req, res) => {
-    
-     res.json([{title: 'taco', text: 'tuesday'}])// placeholder
+    console.log(parsedNotes);
+    res.json([{title: 'taco', text: 'tuesday'}])// placeholder
 });
 
 router.post('/notes', (req, res) => { 

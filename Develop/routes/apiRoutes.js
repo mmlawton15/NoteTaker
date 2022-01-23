120800c31e56c3457633b58c5ebf2f591a09eae4
Mary Margaret <mmlawton15@gmail.com>
2022-01-23 10:40:34 -0500

    updating pseudocode

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const fs = require("fs");
 
-//FUNCTION TO RETURN PRE-EXISTING NOTES
+//FUNCTION TO GET PRE-EXISTING NOTES AND SHOW ON HTML
 router.get('/notes', (req, res) => {
     //read the db every time you hit a route, otherwise i will only be dealing with old notes and reading it when the server turns on
     let preloadedNotes = fs.readFileSync('./db/db.json');
@@ -14,7 +14,7 @@ router.get('/notes', (req, res) => {
 });
 
 
-
+//FUNCTION TO CHANGE THE DB.JSON FILE AND PUT THE CHANGED RESULT ON HTML
 router.post('/notes', (req, res) => { 
     //reading the current request body
     console.log(req.body); 

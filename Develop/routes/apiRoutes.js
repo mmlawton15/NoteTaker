6412eabdb4c5eff7298e8931272495d942ec1c85
Mary Margaret <mmlawton15@gmail.com>
2022-01-23 11:02:40 -0500

    i know i am so close to finguring this out! Calling in backup

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -16,11 +16,23 @@ router.get('/notes', (req, res) => {
 
 //FUNCTION TO CHANGE THE DB.JSON FILE AND PUT THE CHANGED RESULT ON HTML
 router.post('/notes', (req, res) => { 
-    //reading the current request body
+    //reading the current request body in terminal
     console.log(req.body); 
-    //need it to display the req body in the html
+    //need it to change the db.json file via write file. add the new note to the array
 
-    //end resonse without providing data, placeholder for now
+        //then write to file
+        fs.writeFile('./db/db.json', (req.body).toString(), function(err) { 
+            if (err) { //if there is an error 
+                console.log("Error: " + err); //console log this error
+                return;
+            }
+        })
+    
+
+
+    //after the new note is in db.json, make it display on html
+
+    //this will end resonse without providing data, placeholder for now
     res.end() 
 });
 

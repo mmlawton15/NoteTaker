9cb54b7c3e2c257471469eabf294805de6e3e88e
Mary Margaret <mmlawton15@gmail.com>
2022-01-23 10:37:02 -0500

    almost done with this challenge, gotta get the newly saved note into the db.json file and to display on the html page

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,30 +1,27 @@
 const router = require('express').Router()
 const fs = require("fs");
 
-
-// someone is going to be calling for data at this endpoint
-// give them the data
-// how
-// where are the notes - db.json
-// reac into the db.json file
-// grabe the notes and return the json
-
-
+//FUNCTION TO RETURN PRE-EXISTING NOTES
 router.get('/notes', (req, res) => {
     //read the db every time you hit a route, otherwise i will only be dealing with old notes and reading it when the server turns on
     let preloadedNotes = fs.readFileSync('./db/db.json');
     //parse the notes to be json
     let parsedNotes = JSON.parse(preloadedNotes);
+    //returning db.json notes
+    console.log(parsedNotes); 
     //return the item
-    
-    console.log(parsedNotes); //returning db.json notes
-    res.json(parsedNotes) //what i am working with
+    res.json(parsedNotes) 
 });
 
+
+
 router.post('/notes', (req, res) => { 
-    console.log(req.body);
-    
-    res.end() //placeholder
+    //reading the current request body
+    console.log(req.body); 
+    //need it to display the req body in the html
+
+    //end resonse without providing data, placeholder for now
+    res.end() 
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -12,8 +12,6 @@
 // WHEN I click on the Write icon in the navigation at the top of the page
 // THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
-
-
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes.js');
